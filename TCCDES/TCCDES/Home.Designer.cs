560f42d5fb660acb41b3c385f999377a45e7cfc7
willotavio <willian.oliveira177@etec.sp.gov.br>
2022-06-07 17:05:44 -0300

    teste

--- TCCDES/TCCDES/Home.Designer.cs
+++ TCCDES/TCCDES/Home.Designer.cs
@@ -28,28 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(147, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(330, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ONG ALEM BRASIL";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(215, 130);
+            this.button1.Location = new System.Drawing.Point(215, 187);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(166, 85);
             this.button1.TabIndex = 1;
@@ -61,7 +52,7 @@
             this.button2.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(215, 237);
+            this.button2.Location = new System.Drawing.Point(215, 294);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(166, 85);
             this.button2.TabIndex = 2;
@@ -73,35 +64,46 @@
             this.button3.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(215, 343);
+            this.button3.Location = new System.Drawing.Point(215, 400);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(166, 85);
             this.button3.TabIndex = 3;
             this.button3.Text = "CONTA";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::TCCDES.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 140);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TCCDES.Properties.Resources.wallpaper;
             this.ClientSize = new System.Drawing.Size(610, 541);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "Home";
             this.Text = "Ong Alem Brasil";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
